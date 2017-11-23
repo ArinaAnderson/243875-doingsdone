1323b7ce380cc48dd7a7323c227c45ab40cc9b89
ArinaAnderson <iolsoOfuolso@gmail.com>
2017-11-23 12:16:41 -0800

    Исправления по работе с формрй добавления задач

--- index.php
+++ index.php
@@ -3,7 +3,51 @@ date_default_timezone_set('Europe/Moscow');
  
 require_once('functions.php');
 require_once('templates/data.php');
- 
+
+$errors = []; 
+$bodyClass = "";
+$errorMessage = 'Заполните это поле';
+$mistakeClass = ' form__input--error';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+    $required = ['name', 'project', 'date'];
+    foreach ($_POST as $key => $value) {
+        if (in_array($key, $required) && $value == '') {
+            $errors[] = $key;
+        }
+    }
+    if (!count($errors)) {
+        if (isset($_FILES['preview']['name'])) { 
+            $fileType = "." . $_FILES['preview']['type'];
+            $res = move_uploaded_file($FILES['preview']['tmp-name'], $_SERVER['DOCUMENT_ROOT']); //сохраняем в корне проекта
+        }
+        
+        $dateInputString = "" . ($_POST['date']);
+        $dateInputTimeMark = strtotime($dateInputString);
+        $dateForm = date("d.m.Y", $dateInputTimeMark);
+        $taskNewArray = [
+            'task' => htmlspecialchars($_POST['name']),
+            'deadline' => htmlspecialchars($dateForm),
+            'type' => htmlspecialchars($_POST['project']),
+            'fileName' => htmlspecialchars($_FILES['preview']['name']),
+            'fileResolution' => htmlspecialchars($fileType)
+        ];
+        array_unshift($tasks, $taskNewArray);
+    }
+}
+
+$formTaskContent = '';
+
+if (isset($_GET['add']) || !empty($errors)) { 
+    $bodyClass = 'overlay';
+    $formTaskContent = getTemplate('templates/add.php', [
+        'mistakeClass' => $mistakeClass,
+        'errorMessage' => $errorMessage,
+        'errors' => $errors,
+        'mainNavigation' => $mainNavigation //для заполнения тега select в шаблоне формы
+    ]);
+}
+
 $taskList = [];
 
 if (isset($_GET['project_id'])) {  
@@ -14,58 +58,12 @@ if (isset($_GET['project_id'])) {
             }
         }
     } else { 
-    	http_response_code(404); 
+        http_response_code(404); 
     }
 } else  {   
     $taskList = $tasks;
 }
 
-$bodyClass = "";
-if (isset($_GET['add'])) { 
-    $bodyClass = 'overlay';
-    $formTaskContent = getTemplate('templates/add.php', [
-        'mistakeClassArray' => $mistakeClassArray,
-        'errorMessageArray' => $errorMessageArray,
-        'mainNavigation' => $mainNavigation //для заполнения тега select в шаблоне формы
-    ]);
-}
-
-
-if ($_SERVER['REQUIRED_METHOD'] == 'POST') { 
-    $required = ['name', 'project', 'date'];
-    $mistakeClassArray = [];
-    $errorMessageArray = [];
-    $errors = [];
-    foreach ($_POST as $key => $value) {
-        $mistakeClassArray = [$key => ''];
-        $errorMessageArray = [$key => ''];
-
-        if (in_array($key, $required) && $value == '') {
-            $errors[] = $key; 
-            $mistakeClassArray[$key] = ' form__input--error'; 
-            $errorMessageArray[$key] = 'Заполните это поле';
-            
-        }
-    }
-    if (!count($errors)) {
-        $bodyClass = '';
-        header ("Location: ../index.php"); //если нет ошибок - убираем окно формы И добавляем данные в массив
-    
-        if (isset($_FILES['preview']['name'])) { 
-            $fileType = "." . $_FILES['preview']['type'];
-            $res = move_uploaded_file($FILES['preview']['tmp-name'], $_SERVER['DOCUMENT_ROOT']); //сохраняем в корне проекта
-        }
-    
-        $taskNewArray = [
-            'task' => htmlspecialchars($_POST['name']),
-            'deadline' => tmlspecialchars($_POST['date']),
-            'type' => htmlspecialchars($_POST['project']),
-            'fileName' => htmlspecialchars($_FILES['preview']['name']),
-            'fileResolution' => htmlspecialchars($fileType)
-        ];
-        array_unshift($tasks, $taskNewArray);
-    }
-}
 $pageContent = getTemplate('templates/index.php', [
     'tasks' => $taskList
 ]); 
--- templates/add.php
+++ templates/add.php
@@ -1,44 +1,64 @@
 ﻿<div class="modal">
-    <button class="modal__close" type="button" name="button" enctype="multipart/form-data">Закрыть</button>
+    <button class="modal__close" type="button" name="button" >Закрыть</button>
 
     <h2 class="modal__heading">Добавление задачи</h2>
 
-    <form class="form"  action="../index.php" method="post">
+    <form class="form"  action="/index.php" method="post" enctype="multipart/form-data">
         <div class="form__row">
             <label class="form__label" for="name">Название <sup>*</sup></label>
+            <?php if (in_array('name', $errors)):?>
             <p class="form__message">
-                <?=$errorMessageArray['name']?>
+                <?=$errorMessage?>
             </p>
-            <input class="form__input<?=$mistakeClassArray['name']?>" type="text" name="name" id="name" value="" placeholder="Введите название">
+            <?php endif; ?>
+            <input class='form__input
+            <?php 
+            if (in_array('name', $errors)) {
+                echo $mistakeClass;
+            }
+            ?>'
+            type="text" name="name" id="name" value="" placeholder="Введите название">
         </div>
 
         <div class="form__row">
             <label class="form__label" for="project">Проект <sup>*</sup></label>
+            <?php if (in_array('project', $errors)):?>
             <p class="form__message">
                 <?=$errorMessage?>
             </p>
-            <p class="form__message">
-                <?=$errorMessageArray['project']?>
-            </p>
-            <select class="form__input form__input--select<?=$mistakeClassArray['project']?>" name="project" id="project">
+            <?php endif; ?>
+            <select class='form__input form__input--select
+            <?php 
+            if (in_array('project', $errors)) {
+                echo $mistakeClass;
+            }
+            ?>'
+            name="project" id="project">
                 <?php foreach ($mainNavigation as $item):?>
-                <option value="">
-                    <?php
-                    if (!($item == 'Все')) {
-                        echo $item;
-                    }
-                    ?>   
+                <?php if (!($item == 'Все')):?>
+                <option value="<?=$item?>">
+                    <?=$item?>  
                 </option>
+                <?php endif; ?>
                 <?php endforeach?>
             </select>
         </div>
-
+        
         <div class="form__row">
             <label class="form__label" for="date">Дата выполнения</label>
+            <?php if (in_array('date', $errors)):?>
             <p class="form__message">
-                <?=$errorMessageArray['date']?>
+                <?=$errorMessage?>
             </p>
-            <input class="form__input form__input--date<?=$mistakeClassArray['date']?>" type="date" name="date" id="date" value="" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+            <?php endif; ?>
+            <input class='form__input form__input--date
+            <?php 
+            if (in_array('date', $errors)) {
+                echo $mistakeClass;
+            }
+            ?>'
+            type="date" name="date" id="date" value="" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+
         </div>
 
         <div class="form__row">
