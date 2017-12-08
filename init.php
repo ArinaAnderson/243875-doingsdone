eb3a8eeb7149b3ba388c3bdb1f102ae0401b08a2
ArinaAnderson <iolsoOfuolso@gmail.com>
2017-12-08 09:08:49 -0800

    Подключение к Mysql

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php 
+date_default_timezone_set('Europe/Moscow');
+ 
+require_once('functions.php');
+
+error_reporting(0);
+$link = mysqli_connect('89.108.98.195', 'root', 'ArinaAnderson123!', 'doingsdone');
+if (!$link) {
+    $mistake = mysqli_connect_error();
+    print getTemplate('templates/error.php', ['mistake' => $mistake]);
+    die();
+}
+
+error_reporting(-1);
+mysqli_set_charset($link, "utf8");
